01d4c450c72665acce56e79129042e5932375bc8
Shwetha8861 <129156600+Shwetha8861@users.noreply.github.com>
2023-07-14 16:22:08 +0530

    Update views.py
    This is my commit from dockers view

--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, everyone')
-
+    return HttpResponse('Hello, everyone my name is shwetha, nice to meet you')
